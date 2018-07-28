cec9fb80171093c08c150ed5f90bfb9ea600341a
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-28 15:51:59 -0600

    First working-ish example.

--- converter-demo.py
+++ converter-demo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import json
+
 from quart import Quart, websocket, render_template
 from crk_orthography import (
     sro2syllabics, syllabics2sro, __version__ as library_version
@@ -18,7 +20,26 @@ async def hello():
 @app.websocket('/ws')
 async def ws():
     while True:
-        await websocket.send('hello')
+        data = await websocket.receive()
+        await websocket.send(handle_request(data))
+
+
+def jsonify(fn):
+    def wrapped(raw_data, *args, **kwargs):
+        parsed_data = json.loads(raw_data)
+        raw_response = fn(parsed_data, *args, **kwargs)
+        return json.dumps(raw_response)
+    return wrapped
+
+
+@jsonify
+def handle_request(data):
+    if 'sro' in data:
+        return {'syl': sro2syllabics(data['sro'])}
+    elif 'syl' in data:
+        return {'sro': syllabics2sro(data['syl'])}
+    else:
+        return {'error': 'invalid request'}
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,1 +1,22 @@
-console.log("ready.");
+var ws = new WebSocket(
+  'ws://' + document.domain + ':' + location.port + '/ws'
+);
+
+document.addEventListener('DOMContentLoaded', function () {
+  var sroBox = document.getElementById('sro');
+  var sylBox = document.getElementById('syl');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+    if (data.sro) {
+      sroBox.innerText = data.sro;
+    }
+    if (data.syl) {
+      sylBox.innerText = data.syl;
+    }
+  };
+
+  ws.onopen = function() {
+    ws.send(JSON.stringify({sro: 'maskekosihk trail'}));
+    ws.send(JSON.stringify({syl: '→ ᒪᐢᑫᑯᓯᕽ  ᑎᕒᐁᕀᓬ'}));
+  };
+});
