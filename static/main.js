6669307029a0a88d01eeeeaa80445f7642ae45e6
Eddie Antonio Santos <easantos@ualberta.ca>
2018-07-30 15:03:55 -0600

    Refresh the 'dirty' text field on page load.

--- static/main.js
+++ static/main.js
@@ -15,78 +15,95 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var ws = new ReconnectingWebSocket(
-  (location.protocol == 'https:' ? 'wss' : 'ws') + '://' +
-  document.domain + ':' + location.port + '/ws'
-);
+(function () {
 
-document.addEventListener('DOMContentLoaded', function () {
-  var sroBox = document.getElementById('sro');
-  var sylBox = document.getElementById('syl');
+  var dirty = null;
+  var ws = new ReconnectingWebSocket(
+    (location.protocol == 'https:' ? 'wss' : 'ws') + '://' +
+    document.domain + ':' + location.port + '/ws'
+  );
 
-  // Convert SRO as soon as the WebSocket is open.
-  ws.onopen = function() {
-    // XXX: send only the value of SRO to be converted.
-    send({sro: sroBox.value});
-  };
+  document.addEventListener('DOMContentLoaded', function () {
+    var sroBox = document.getElementById('sro');
+    var sylBox = document.getElementById('syl');
 
-  ws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-    updateBoxes(data);
-  };
+    // Convert "dirty" changes soon as the WebSocket is open.
+    ws.onopen = function() {
+      if (dirty == 'sro') {
+        sendSRO();
+      } else if (dirty == 'syl') {
+        sendSyllabics();
+      }
+    };
 
-  sroBox.addEventListener('input', function (event) {
-    send({sro: event.target.value});
-  });
+    ws.onmessage = function (event) {
+      var data = JSON.parse(event.data);
+      updateBoxes(data);
+    };
 
-  sylBox.addEventListener('input', function (event) {
-    send({syl: event.target.value});
-  });
-
-  function updateBoxes(data) {
-    if (data.sro)
-      sroBox.value = data.sro;
-    if (data.syl)
-      sylBox.value = data.syl;
-  }
-
-  function send(message) {
-    ws.send(JSON.stringify(message));
-  }
-});
-
-window.getDefaultTextareaValue = function (name) {
-  var textarea = document.getElementById(name);
-  const defaults = {
-    sro: "tân'si",
-    syl: 'ᑖᓂᓯ'
-  };
-
-  if (name != 'sro' && name != 'syl') {
-    return;
-  }
-
-  let fragment = parseFragment();
-  if (fragment[name]) {
-    textarea.value = fragment[name];
-  } else {
-    textarea.value = defaults[name];
-  }
-
-  function parseFragment() {
-    var fragment = location.hash;
-    let [_, pairsText] = fragment.split('!', 2);
-    if (!pairsText) {
-      return {};
-    }
-
-    const pairs = {};
-    pairsText.split(';').forEach(pair => {
-      let [key, value] = pair.split(':', 2);
-      pairs[key] = decodeURIComponent(value);
+    sroBox.addEventListener('input', function (event) {
+      send({sro: event.target.value});
     });
 
-    return pairs;
-  }
-};
+    sylBox.addEventListener('input', function (event) {
+      send({syl: event.target.value});
+    });
+
+    function updateBoxes(data) {
+      if (data.sro)
+        sroBox.value = data.sro;
+      if (data.syl)
+        sylBox.value = data.syl;
+    }
+
+    function sendSRO() {
+      send({sro: sroBox.value});
+    }
+
+    function sendSyllabics() {
+      send({syl: sylBox.value});
+    }
+
+    function send(message) {
+      ws.send(JSON.stringify(message));
+    }
+  });
+
+  window.getDefaultTextareaValue = function (name) {
+    var textarea = document.getElementById(name);
+    const defaults = {
+      sro: "tân'si",
+      syl: 'ᑖᓂᓯ'
+    };
+
+    if (name != 'sro' && name != 'syl') {
+      return;
+    }
+
+    let fragment = parseFragment();
+    if (fragment[name]) {
+      dirty = name;
+      textarea.value = fragment[name];
+    } else {
+      textarea.value = defaults[name];
+    }
+
+    function parseFragment() {
+      var fragment = location.hash;
+      let [_, pairsText] = fragment.split('!', 2);
+      if (!pairsText) {
+        return {};
+      }
+
+      const pairs = {};
+      pairsText.split(';').forEach(pair => {
+        let [key, value] = pair.split(':', 2);
+        pairs[key] = decodeURIComponent(value);
+      });
+
+      return pairs;
+    }
+  };
+
+}());
 /*global ReconnectingWebSocket*/
