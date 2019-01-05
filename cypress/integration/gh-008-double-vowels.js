c16e1469f1fd6b11640bca72febf30a8ad920fa1
Eddie Antonio Santos <easantos@ualberta.ca>
2019-01-05 15:18:06 -0700

    Test double-vowels on copy-paste.

--- cypress/integration/gh-008-double-vowels.js
+++ cypress/integration/gh-008-double-vowels.js
@@ -7,17 +7,41 @@ describe('Double typing for long vowels', function () {
 
     cy.get('textarea#sro')
       .clear()
+    // type double vowels here:
       .type('eetii niiya, eekwa oohoo niciihkeeyimaaw.');
 
+    // it should convert properly
     cy.get('textarea#syl')
       .invoke('val')
       .should('equal', 'ᐁᑏ ᓃᔭ, ᐁᑿ ᐆᐦᐆ ᓂᒌᐦᑫᔨᒫᐤ᙮')
 
+    // ...and the results should be reflected in the original buffer.
     cy.get('textarea#sro')
       .invoke('val')
       .should('contain', 'êtî nîya, êkwa ôhô nicîhkêyimâw.');
   });
 
-  it.skip('should not affect pasted text');
+  it('should not affect pasted text', function () {
+    cy.get('textarea#sro').as('sro');
+    var insertedText = 'mitho-ociimi-kiisikanisik';
+
+    // Typing out the text should replace double vowels.
+    cy.get('@sro')
+      .clear()
+      .type(insertedText)
+      .invoke('val')
+      .should('not.contain', insertedText)
+      .and('contains', 'mitho-ocîmi-kîsikanisik');
+
+    // Pretend to paste text into the box:
+    cy.get('@sro')
+      .clear()
+      .invoke('val', insertedText)
+      .trigger('input')
+    // However, this should NOT change the buffer!
+      .invoke('val')
+      .should('contain', insertedText);
+  });
+
   it.skip('can be disabled');
 });
