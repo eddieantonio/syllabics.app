554833624ee663bdc7ea2fe819f8bc46d119a867
Eddie Antonio Santos <easantos@ualberta.ca>
2019-02-03 21:01:11 -0700

    Change initial text to placeholder.

--- cypress/integration/000-basic-functionality.js
+++ cypress/integration/000-basic-functionality.js
@@ -18,10 +18,34 @@ describe('Basic functionality', function () {
 
     cy.get('textarea#syl')
       .clear()
-      .type('ᑲᐦᑭᔭᐤ ᑮᑿᕀ ᐚᐦᑰᐦᑐᐏᐤ᙮');
+      .type('ᑲᐦᑭᔭᐤ ᑮᑿᕀ ᐚᐦᑰᐦᑐᐏᐣ᙮');
 
     cy.get('textarea#sro')
       .invoke('val')
-      .should('equal', 'kahkiyaw kîkway wâhkôhtowiw.');
+      .should('equal', 'kahkiyaw kîkway wâhkôhtowin.');
+  });
+
+  it('allows for SRO transcription without clearing the textarea', function () {
+    cy.visit('/');
+
+    cy.get('textarea#sro')
+      .click()
+      .type("tapwe, miyo-kîsikâw anohc.");
+
+    cy.get('textarea#syl')
+      .invoke('val')
+      .should('equal', `ᑕᐻ, ᒥᔪ${NNBSP}ᑮᓯᑳᐤ ᐊᓄᐦᐨ᙮`);
+  });
+
+  it('allows for syllabics transcription without clearing the textarea', function () {
+    cy.visit('/');
+
+    cy.get('textarea#syl')
+      .clear()
+      .type('ᑲᐦᑭᔭᐤ ᑮᑿᕀ ᐚᐦᑰᐦᑐᐏᐣ᙮');
+
+    cy.get('textarea#sro')
+      .invoke('val')
+      .should('equal', 'kahkiyaw kîkway wâhkôhtowin.');
   });
 });
